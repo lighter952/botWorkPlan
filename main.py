32edbc8380c806f31fcc5a7cc482fa5a53e44a3a
Holias <user@macbook-pro-holias.home>
2023-04-09 22:28:07 +0200

    Add full mounth reading

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from table_parsing import get_list_of_employees
 from table_parsing import get_plan_by_employee_number
+
 file_name = "plan.xlsx"
 
-
 employees_list = get_list_of_employees(file_name)
-
 i = 0
 for employee in employees_list:
     i += 1
     print(str(i) + ' - ' + employee)
+print("Choose your name: ")
 inpt = int(input()) - 1
-employee_number = employees_list.get(list(employees_list.keys())[inpt])
+
+employee_number: list = employees_list.get(list(employees_list.keys())[inpt])
 employee_plan = get_plan_by_employee_number(employee_number, file_name)
 
 for shift in employee_plan:
@@ -18,3 +19,7 @@ for shift in employee_plan:
         print(str(shift.day_of_month) + ": Weekend!")
         continue
     print(str(shift.day_of_month) + ': ' + str(shift.start_time) + ' - ' + str(shift.end_time))
+
+
+
+
--- table_parsing.py
+++ table_parsing.py
@@ -11,22 +11,18 @@ def get_list_of_employees(file_name) -> dict:
         i += 1
         table_cell = db.ws(ws='04.2023 ').index(row=i, col=3)
         if len(table_cell) > 10 and table_cell != 'godziny':
-            employee_list[str(table_cell)] = i
+            employee_list[str(table_cell)] = [i, i + 62]
     return employee_list
 
 
-def get_plan_by_employee_number(number: int, file_name: str) -> list:
+def get_plan_by_employee_number(number: list, file_name: str) -> list:
     db = xl.readxl(file_name)
     list_of_days = list()
     i = 3
-    day_of_month = 1
-    shift_start_time = datetime.now()
-    shift_end_time = datetime.now()
-    is_weekend = False
 
     while i < 33:  # 33 is summary longitude of table
         i += 1
-        cell = db.ws(ws='04.2023 ').index(row=number, col=i)
+        cell = db.ws(ws='04.2023 ').index(row=number[0], col=i)
 
         if i % 2 == 0:
             day_of_month = int(db.ws(ws='04.2023 ').index(row=5, col=i))
@@ -45,4 +41,25 @@ def get_plan_by_employee_number(number: int, file_name: str) -> list:
                 is_weekend = True
                 list_of_days.append(Shift(day_of_month, shift_start_time, shift_end_time, is_weekend))
 
+    i = 3
+    while i < 33:  # 33 is summary longitude of table
+        i += 1
+        cell = db.ws(ws='04.2023 ').index(row=number[1], col=i)
+
+        if i % 2 == 0:
+            day_of_month = int(db.ws(ws='04.2023 ').index(row=68, col=i))
+
+        if len(str(cell)) > 1:
+            is_weekend = False
+            datetime_object = datetime.strptime(cell, '%H:%M:%S')
+            if i % 2 == 0:
+                shift_start_time = datetime_object.time()
+            else:
+                shift_end_time = datetime_object.time()
+                list_of_days.append(Shift(day_of_month, shift_start_time, shift_end_time, is_weekend))
+        else:
+            if i % 2 == 0:
+                shift_start_time = shift_end_time = datetime.strptime("00:00:00", '%H:%M:%S')
+                is_weekend = True
+                list_of_days.append(Shift(day_of_month, shift_start_time, shift_end_time, is_weekend))
     return list_of_days
